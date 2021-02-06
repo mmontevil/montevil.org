71a44f92d2dbd552763f732ffeb0d1ca64068784
Maël Montévil <mael.montevil@gmail.com>
2021-02-06 04:41:20 +0100

    hp

--- .eleventy.js
+++ .eleventy.js
@@ -93,7 +93,7 @@ eleventyConfig.addPlugin(readingTime);
 
 const pluginTOC = require('eleventy-plugin-nesting-toc')
  
-  eleventyConfig.addPlugin(pluginTOC,{tags:['h2', 'h3', 'h4', 'h5', 'h6'], wrapperClass:'indent autotoc'})
+  eleventyConfig.addPlugin(pluginTOC,{tags:['h3', 'h4', 'h5', 'h6'], wrapperClass:' autotoc indent '})
 
 
   // ------------------------------------------------------------------------
