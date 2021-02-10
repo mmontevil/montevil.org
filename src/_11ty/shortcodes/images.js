96618e8f707544a037265fb7d39dfbd49d910ad2
Maël Montévil <mael.montevil@gmail.com>
2021-02-10 13:50:02 +0100

    mm

--- src/_11ty/shortcodes/images.js
+++ src/_11ty/shortcodes/images.js
@@ -1,6 +1,5 @@
 module.exports = {
-  icon: (id, label) => `<svg class="icon" role="img" aria-label="${label}" focusable="false"><use xlink:href="#symbol-${id}" /></svg>
-`,
+  icon: (id, label) => `<svg class="icon" role="img" aria-label="${label}" focusable="false"><use xlink:href="#symbol-${id}" /></svg>`,
   archiveIllustration: (src, width, height, alt) => {
     let image = `
 <img
