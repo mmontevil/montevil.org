6113426a115d4ba62fcb0b26a67c3f5a8f6c762d
Maël Montévil <mael.montevil@gmail.com>
2021-02-28 04:21:50 +0100

    more fixes, changed og image

--- src/_11ty/shortcodes/opengraph.js
+++ src/_11ty/shortcodes/opengraph.js
@@ -30,8 +30,8 @@ module.exports = {
   }else{ let buff =  Buffer.from(image);
 let base64data = buff.toString('base64');
     if(tagline ===''){
-        return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_500/fl_layer_apply,x_-300,y_0/w_520,c_fit,co_rgb:000000,g_south_west,x_580,y_300,l_text:Georgia_60:'+encodeURIComponent(title).replace('%2C','')+'/https://montevil.org/assets/blue4.webp';
+        return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_500,h_500,c_limit/fl_layer_apply,x_-320,y_0/w_600,c_fit,co_rgb:691449,g_west,x_580,y_-100,l_text:Georgia_55:'+encodeURIComponent(title).replace('%2C','')+'/w_600,c_fit,co_rgb:a36c89,g_north_west,x_50,y_550,l_text:Georgia_40:'+encodeURIComponent('Maël Montévil')+'/https://montevil.org/assets/limbes.jpg';
     }else{
-  return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_400/fl_layer_apply,x_-360,y_0/w_720,c_fit,co_rgb:000000,g_north_west,x_480,y_100,l_text:Georgia_60:'+encodeURIComponent(title).replace('%2C','')+'/w_720,c_fit,co_rgb:691449,g_south_west,x_480,y_100,l_text:Georgia_40:'+encodeURIComponent(tagline)+'/https://montevil.org/assets/blue4.webp';
+  return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_400/fl_layer_apply,x_-360,y_0/w_720,c_fit,co_rgb:691449,g_north_west,x_480,y_100,l_text:Georgia_60:'+encodeURIComponent(title).replace('%2C','')+'/w_720,c_fit,co_rgb:ede1e6,g_south_west,x_480,y_70,l_text:Georgia_40:'+encodeURIComponent(tagline)+'/https://montevil.org/assets/limbes.jpg';
 }}}
 };
