d6349fbe06e11685c5664eddc61752971a9bb951
Maël Montévil <mael.montevil@gmail.com>
2021-03-07 18:32:30 +0100

    og images

--- src/_11ty/shortcodes/opengraph.js
+++ src/_11ty/shortcodes/opengraph.js
@@ -33,9 +33,16 @@ module.exports = {
     
     let buff =  Buffer.from(image);
 let base64data = buff.toString('base64');
-    if(tagline ===''){
-        return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_500,h_500,c_limit/fl_layer_apply,x_-320,y_0/w_600,c_fit,co_rgb:691449,g_west,x_580,y_-100,l_text:Georgia_55:'+encodeURIComponent(title).replace('%2C','')+'/w_600,c_fit,co_rgb:a36c89,g_north_west,x_50,y_550,l_text:Georgia_40:'+encodeURIComponent('Maël Montévil')+'/https://montevil.org/assets/limbes.jpg';
+ /*   if(tagline ===''){
+        return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_400,h_350,c_limit/fl_layer_apply,x_-360,y_-125,e_shadow:90/w_520,c_fit,co_rgb:691449,g_north_west,x_680,y_50,l_text:Georgia_40_bold:'+encodeURIComponent(title).replaceAll('%2C','')+',e_shadow:90/w_600,c_fit,co_rgb:a36c89,g_south_west,x_150,y_30,l_text:Georgia_40:'+encodeURIComponent('Maël Montévil')+'/https://montevil.org/assets/limbes2.jpg';
     }else{
-  return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_400/fl_layer_apply,x_-360,y_0/w_720,c_fit,co_rgb:691449,g_north_west,x_480,y_100,l_text:Georgia_60:'+encodeURIComponent(title).replace('%2C','')+'/w_720,c_fit,co_rgb:ede1e6,g_south_west,x_480,y_70,l_text:Georgia_40:'+encodeURIComponent(tagline)+'/https://montevil.org/assets/limbes.jpg';
-}}}
+  return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/l_fetch:'+base64data+',w_400,h_400,c_limit/fl_layer_apply,x_-360,y_-125/w_520,c_fit,co_rgb:691449,g_north_west,x_680,y_50,l_text:Georgia_40_bold:'+encodeURIComponent(title).replaceAll('%2C','')+'/w_1100,c_scale,co_rgb:ede1e6,g_south_west,x_50,y_30,l_text:Georgia_40:'+encodeURIComponent(tagline)+'/https://montevil.org/assets/limbes2.jpg';
+}*/
+    if(tagline ===''){
+        return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/w_520,c_fit,co_rgb:691449,g_north_west,x_680,y_50,l_text:Georgia_40_bold:'+encodeURIComponent(title).replaceAll('%2C','%252C')+',e_shadow:90/w_600,c_fit,co_rgb:ede1e6,g_south_west,x_150,y_30,l_text:Georgia_40:'+encodeURIComponent('Maël Montévil')+'/https://montevil.org/assets/limbes2.jpg';
+    }else{
+  return 'https://res.cloudinary.com/mmontevil/image/fetch/w_1200,h_630,c_fill,q_auto,f_auto/w_520,c_fit,co_rgb:691449,g_north_west,x_680,y_50,l_text:Georgia_40_bold:'+encodeURIComponent(title).replaceAll('%2C','%252C')+',e_shadow:90/w_1100,c_limit,co_rgb:ede1e6,g_south_west,x_50,y_30,l_text:Georgia_40:'+encodeURIComponent(tagline).replaceAll('%2C','%252C')+'/https://montevil.org/assets/limbes2.jpg';
+}
+    
+  }}
 };
