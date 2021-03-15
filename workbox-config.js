004715c7bebdcd4f5935274227219191f0a52272
Maël Montévil <mael.montevil@gmail.com>
2021-03-15 14:03:01 +0100

    header

--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "globDirectory": "_site/",
-  "globPatterns": [
-    "**/*.{svg,html,png,jpg,bib,webp,woff,woff2,ttf,js,css,jpeg,xml,json,webmanifest,mov}"
-  ],
-  "swDest": "_assets/js/sw.js"
-};
