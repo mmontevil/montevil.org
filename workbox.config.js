064b5e1c1d3974005bae4d87fa90c2c332a08508
Maël Montévil <mael.montevil@gmail.com>
2021-03-15 14:38:10 +0100

    wb

--- workbox.config.js
+++ workbox.config.js
@@ -6,13 +6,16 @@ module.exports = {
   globDirectory: DIST,
   globPatterns: [
     './js/additional-es.*.js',
-    './',
+    './index.html',
     './about/',
     './about/the-website.html',
     './offline.html',
     './offline-fallback.html',
     './manifest.webmanifest',
-    './publications/articles/2020*.html'
+    './publications/articles/2020*.html',
+    './publications/chapter/2020*.html',
+    './assets/me/Montevil-192px.png',
+    './assets/me/Montevil-512px.png'
   ],
   //dontCacheBustURLsMatching: new RegExp('.+.[a-f0-9]{8}..+'),
   //swSrc: path.join(DIST, 'service-worker.js'),
