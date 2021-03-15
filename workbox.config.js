f6c4c5ed7ca334ac064f80a6090c1cc971bba9d4
Maël Montévil <mael.montevil@gmail.com>
2021-03-15 15:34:16 +0100

    sw

--- workbox.config.js
+++ workbox.config.js
@@ -17,6 +17,25 @@ module.exports = {
     './assets/me/Montevil-192px.png',
     './assets/me/Montevil-512px.png'
   ],
+  runtimeCaching: [
+    {
+      // Match any request ends with .png, .jpg, .jpeg or .svg.
+      urlPattern: /.(.*)$/,
+
+      // Apply a cache-first strategy.
+      handler: "NetworkFirst",
+
+      options: {
+        // Use a custom cache name.
+        cacheName: "images",
+
+        // Only cache 10 images.
+        expiration: {
+          maxEntries: 100
+        }
+      }
+    }
+  ],
   //dontCacheBustURLsMatching: new RegExp('.+.[a-f0-9]{8}..+'),
   //swSrc: path.join(DIST, 'service-worker.js'),
   swDest: path.join(DIST, 'service-worker.js'),
