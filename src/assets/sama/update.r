59456245217be202ae06b31605e89dff6ff303b6
Maël Montévil <mael.montevil@gmail.com>
2023-02-07 12:07:22 +0100

    update

--- src/assets/sama/update.r
+++ src/assets/sama/update.r
@@ -2,7 +2,7 @@
 #source("/home/kamome/Work/cana/confocal3Dculture/t47d/install.r")
 #install.packages("./Rsama_0.77.tar.gz")
 #
-version=0.997;
+version=0.9971;
 file=paste("Rsama_",version,".tar.gz",sep="");
 download.file(paste("https://montevil.org/assets/sama/",file,sep=""),file);
 install.packages(file,repos = NULL,type="source")
